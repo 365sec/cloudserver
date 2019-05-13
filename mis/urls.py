009258043e8dbf9fa151e5c084282e710db7348a
jay900323 <641211396@qq.com>
2019-05-13 18:55:49 +0800

    fix url.py、link.py

--- mis/urls.py
+++ mis/urls.py
@@ -1,31 +1,16 @@
-"""gov URL Configuration
+#coding: utf-8
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 from t import views
 from t import link
-from . import view
+from t import login
 
 urlpatterns = [
-    url(r'^data_count$', views.data_count),
+    url(r'^data_count$', views.view_report),
     url(r'^agent/query/$', views.agent_query),
     url(r'^attack/query/$', views.attack_event_query),
     url(r'^attack/query_source/$', views.attack_query_source),
-    url(r'^download$', view.agent_download),
-    url(r'^agentClick$', view.agent_click),
     url(r'^query_threat_level$', views.query_threat_level),
     url(r'^query_attack_source$', views.query_attack_source),
     url(r'^query_attack_times$', views.query_attack_times),
@@ -34,16 +19,19 @@ urlpatterns = [
     url(r'^plugins$', views.plugins_manage),
     url(r'^plugins_update$', views.plugins_update),
     url(r'^add_host$', views.add_host),
-    url(r'^login$', views.login),
-    url(r'^logout$', views.loginout),
-    url(r'^index$', views.index),
+    url(r'^login$', login.login),
+    url(r'^logout$', login.loginout),
+    url(r'^index$', login.index),
+    url(r'^download$', link.download),
+    url(r'^agentClick$', link.agent_click),
     url(r'^overview$', link.overview),
     url(r'^attack$', link.attack),
-    url(r'^manage$', link.manage),
+    url(r'^agent$', link.agent),
     url(r'^countreport$', link.countreport),
     url(r'^agent_download$', link.agent_download),
     url(r'^agent_detail$', link.agent_detail),
     url(r'^manage_detail$', link.manage_detail),
-    url(r'', views.index),
+    url(r'^manage$', link.manage),
+    url(r'', login.index),
 ]
 
